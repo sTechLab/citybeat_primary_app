50a216c875619fdd67641cc311f8a8bd56a8bc9a
Alexandra Orth <alexandra.orth@me.com>
2013-09-09 16:23:18 -0400

    many, many changes

--- myrestapi.py
+++ myrestapi.py
@@ -2,6 +2,8 @@ import json
 import datetime
 import bottle
 import time
+import sys
+import shutil
 from bottle import route, run, request, abort, template, static_file
 from pymongo import Connection
 from bson.objectid import ObjectId
@@ -9,68 +11,68 @@ from bson.objectid import ObjectId
 connection = Connection('grande.rutgers.edu', 27017)
 db = connection['citybeat_production']
 
-@route('/')
-def index():
-    output = template('index.html')
-    return output
+mayor_race = [["@Quinn4NY", "@ChrisCQuinn", "#quinn"], ["@BilldeBlasio", "@deblasionyc", "blasio", "#TeamdeBlasio", "#deblasio"],["@billthompsonnyc"], ["@anthonyweiner", "#anthonyweiner", "anthonyweiner"], ["@johncliu", "@JohnLiu2013", "#liu"], ["#albanese", "@salalbanese2013"]]
+comptroller_race = [["@stringer2013", "#stringer", "@scottmstringer"], ["@spitzer2013", "#spitzer"]];
+public_advocate_race = [["@reshmasaujani", "#saujani", "saujani"], ["@squadron4NY", "@danielsquadron"], ["@tish2013", "@tishjames", " #teamtish", "#tish"]];
+manhatten_president_race = [["@galeforMBP", "@galeabrewer", "#gale", "#galebrewer"], ["@juliemenin", "#menin", "menin"], ["@jesslappin", "#lappin", "lappin"], ["@RJackson_NYC"]];
+queens_president_race = [["@melindakatz", "#katz"], ["@pfvjr", "#vallone"]];
+brooklyn_da_race = [["@hynesforda", "#hynes", "@brooklynda"], ["@KenThompson4DA", "#kenthompson"]];
+repub_mayor_primary_race = [["@joelhota", "@joelhota4mayor"], ["@jcats2013"], ["@mcdonald4nyc"]];
 
-@route('/tweets/:id', method='GET')
-def get_document(id):
-    ids = id.split("@")
-    print "these are the ids"
-    print ids
+print sys.argv
 
-    ids.pop(0)
-    print ids
+def get_document(list_name, ids):
+    while True:
+        print "60 SECONDS HAVE PASSED"
+        print ids
+        print list_name
+        open("static/" + list_name + "_updating_2.json", 'w').close()
+        f = open("static/" + list_name + "_updating_2.json", "w")
 
-    entities= ["["]
+        timestamp = str(round(int(time.time()), 0) - 3600)
+        timestamp = timestamp.replace(' ', '')[:-2].upper()
+        print timestamp
 
-    for id in ids:
-        print "went in"
-        # for post in db['tweets'].find({'text': {'$regex':'^(?=.*'+ id + ').*'}}):
-        #     entities.append(json.dumps(post))
-        #     entities.append(",")
-        for post in db['photos'].find({'caption.text': {'$regex':'^(?=.*'+ id + ').*'}}):
-            entities.append(json.dumps(post))
-            entities.append(",")
-            print "INSTAGRAM"
-            print post
+        entities= ["["]
 
-    entities[len(entities)-1] = "]"
-    if not entities:
-        abort(404, 'No document with id %s' % id)
-    return entities
- 
-@route('/static/<filename>')
-def server_static(filename):
-    return static_file(filename, root='static')
+        for id in ids:
+            print id
+            for tag in id:
+                print tag
+                for post in db['tweets'].find({'text': {'$regex': '.*' + tag + '', '$options':'-i'}}):
+                    entities.append(json.dumps(post))
+                    entities.append(",")
+                    print post
+                for post in db['photos'].find({'caption.text': {'$regex': '.*' + tag + '', '$options':'-i'}}):
+                    entities.append(json.dumps(post))
+                    entities.append(",")
+                    print "INSTAGRAM"
+                    print post
 
-@route('/current_tweets', method='GET')
-def get_tweets():
-    print "went in to currnet tweets"
+        entities[len(entities)-1] = "]"
+        if not entities:
+            abort(404, 'No document with id %s' % tag)
+        f.write(" ".join(entities))
+        f.flush()
+        f.close()
 
-    # gen_time = datetime.datetime(2013, 1, 1)
-    # dummy_id = ObjectId.from_datetime(gen_time)
+        shutil.copy2("static/" + list_name + "_updating_2.json", "static/" + list_name + "_2.json")
+        time.sleep(200)
 
-    # print dummy_id
+# def current():
 
-    ti = datetime.datetime.now()
+#     timestamp = str(round(int(time.time()), 0))
+#     print timestamp
+#     entities= ["["]
 
-    end = time.mktime(ti.timetuple())
-    start = time.mktime(ti.timetuple()) - 3600
+#     for post in db['tweets'].find({'created_time': {'$gte': '"' + timestamp + '"'}}):
+#         entities.append(json.dumps(post))
+#         entities.append(",")
+#         print post
 
-    # print start
-    # print end
-
-    entities= ["["]
-
-    for post in db['tweets'].find({ 'created_time' : { '$regex' : ' > 135577899' } }):
-        entities.append(json.dumps(post))
-        entities.append(",")
-        print post
+#     entities[len(entities)-1] = "]"
 
 
-    entities[len(entities)-1] = "]"
-    return entities
+# current()
+get_document(sys.argv[1], eval(sys.argv[1]))
 
-run(host='localhost', port=8000)
