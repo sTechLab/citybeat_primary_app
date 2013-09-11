cf4600f3673e695411b2eec54c20c6c1d6bf9ea2
Adam Krebs <amk528@cs.nyu.edu>
2013-09-10 21:58:46 -0400

    first pass on stylistic tweaks

--- static/map.js
+++ static/map.js
@@ -49,7 +49,7 @@ function set_time() {
   $('#date-day').text(day);
   $('#date').text(date);
 
-  
+
   var time = date_obj.toString("h:mm tt");
   $('#time').text(time);
 }
@@ -96,7 +96,7 @@ setInterval(set_time, 10)
         }
         done();
       });
-    }, 
+    },
     function(done) {
       $.getJSON(location.origin + '/jet/twitter/trending', function(data) {
 
@@ -145,7 +145,7 @@ var data_pulse_prop = function(deviation, scale_function, pulse_function) {
   props.max_scale = scale_function(start_scale, deviation);
 
   props.pulse_time /= deviation;
-  
+
   return props;
 };
 
@@ -190,89 +190,77 @@ var data_pulse_prop = function(deviation, scale_function, pulse_function) {
 L.Map.include(MapCenterOffsetMixin);
 
 
+var getDotColor = function(current_race, text) {
+  var color = '';
+  current_race.some(function(r, i){
+    return r.some(function(name){
+      if (~(text.indexOf(name))) {
+        color = "static/dots/" + colors[i] + "_dot.png"
+        return true;
+      }
+    });
+  });
+  return color;
+};
+
+var getTweetGeoPoint = function(evt, url) {
+  return {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      // x, y is the standard for GeoJSON and many formats
+      coordinates: [evt.location.longitude, evt.location.latitude]
+    },
+    properties: {
+      icon: {
+        iconUrl: url,
+        iconSize: [5, 5]
+      },
+      text: evt.text,
+      person: "@" + evt.user.screen_name,
+      time: evt.created_time,
+      image: ""
+    }
+  };
+};
+
+var getFSQGeoPoint = function(evt, url) {
+  return {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      // x, y is the standard for GeoJSON and many formats
+      coordinates: [evt.location.longitude, evt.location.latitude]
+    },
+    properties: {
+      icon: {
+        iconUrl: url,
+        iconSize: [5, 5]
+      },
+      text: evt.caption.text,
+      person: evt.user.username,
+      time: evt.created_time,
+      image: evt.images.low_resolution.url
+    }
+  };
+}
+
+
 /* Fetch Data, create geo features */
 function fetch_data(current_race, race_name, names_in_race) {
 
   d3.json('static/'+ race_name + '_2.json', function(error,json) {
-    json.forEach(function(evt){
-      var url = "";
-      if(evt.mid_lng == null){ //this is a tweet
-
-      current_race.forEach(function(r, i){
-
-        r.forEach(function(name){
-          if(evt.text){
-            if((evt.text).indexOf(name) > 0){
-              url = "static/dots/"+colors[i]+"_dot.png" 
-            }
-          }
-        });
-      });
-
-      if (url == "") {
-        url="static/dots/orange_dot.png"
-      };
-
-
-        var geoPoint = {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-              // coordinates here are in longitude, latitude order because
-              // x, y is the standard for GeoJSON and many formats
-              'coordinates': [evt.location.longitude,evt.location.latitude]
-            },
-            'properties': {        
-              'icon': {
-                'iconUrl': url,
-                'iconSize': [5, 5]
-              },
-            'text': evt.text,
-            'person': "@" + evt.user.screen_name,
-            'time': evt.created_time,
-            'image': ""
-            }
-          };
-        }
-        else{
-
-        current_race.forEach(function(r, i){
-          r.forEach(function(name){
-            if(evt.caption.text){
-              if((evt.caption.text).indexOf(name) > 0){
-                url = "static/dots/"+colors[i]+"_dot.png" 
-              }
-            }
-          });
-        });
-
-        if (url == "") {
-          url="static/dots/orange_dot.png"
-        };
-
-          var geoPoint = {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-              // coordinates here are in longitude, latitude order because
-              // x, y is the standard for GeoJSON and many formats
-              'coordinates': [evt.location.longitude,evt.location.latitude]
-            },
-            'properties': {        
-              'icon': {
-                'iconUrl': url,
-                'iconSize': [5, 5]
-              },
-            'text': evt.caption.text,
-            'person': evt.user.username,
-            'time': evt.created_time,
-            'image' : evt.images.low_resolution.url
-            }
-          };
-        }
-
-        geoJson.push(geoPoint);
-      });
+    geoJson = json.map(function(evt) {
+      var geoPoint, url = "";
+      if (evt.mid_lng == null) { //this is a tweet
+        url = getDotColor(current_race, evt.text) || "static/dots/orange_dot.png";
+        geoPoint = getTweetGeoPoint(evt, url);
+      } else {
+        url = getDotColor(current_race, evt.caption.text) || "static/dots/orange_dot.png";
+        geoPoint = getFSQGeoPoint(evt, url);
+      }
+      return geoPoint;
+    });
 
 
     d3.select('#event-window').classed('zoom', true);
@@ -284,17 +272,17 @@ function fetch_data(current_race, race_name, names_in_race) {
 
       var popupContent = null;
 
-    var date_obj = new Date(feature.properties.time * 1000);
-    var day = date_obj.toString("dddd");
-    var date = date_obj.toString("MMMM d, yyyy");
-    
-    var time = date_obj.toString("h:mm tt");
+      var date_obj = new Date(feature.properties.time * 1000);
+      var day = date_obj.toString("dddd");
+      var date = date_obj.toString("MMMM d, yyyy");
+
+      var time = date_obj.toString("h:mm tt");
       // Create custom popup content
       var popupContent =  '<a target="_blank" class="popup" href="' + feature.properties.url + '">' +
                           '<img src="' + feature.properties.image + '">' +
                       '   <h2>' + feature.properties.text + '</h2>' +
-                      '   <span id="user">   ' + feature.properties.person + '   </span>' + 
-                      '   <span id="time_tool">' + day + " " + date + " " + time + '</span>' +        
+                      '   <span id="user">   ' + feature.properties.person + '   </span>' +
+                      '   <span id="time_tool">' + day + " " + date + " " + time + '</span>' +
                       '</a>';
 
       // http://leafletjs.com/reference.html#popup
@@ -308,30 +296,29 @@ function fetch_data(current_race, race_name, names_in_race) {
 
     map.markerLayer.on('click', function(e) {
         map.panTo(e.layer.getLatLng());
-
-      console.log("WHALE")
-      var markers = [];
-      this.eachLayer(function(marker) { markers.push(marker); });
-      cycle(markers);
+        // var markers = [];
+        // this.eachLayer(function(marker) { markers.push(marker); });
+        // cycle(markers);
     });
 
-    function cycle(markers) {
-      var i = 0;
-      function run() {
-          // if (++i > markers.length - 1) i = 0;
-          i = Math.floor((Math.random()*markers.length)+1)
-          map.setView(markers[i].getLatLng(), 13);
-          markers[i].openPopup();
-          window.setTimeout(run, 3000);
-      }
-      run();
-  }
+  //   function cycle(markers) {
+  //     var i = 0;
+  //     function run() {
+  //         // if (++i > markers.length - 1) i = 0;
+  //         i = Math.floor((Math.random()*markers.length)+1)
+  //         map.setView(markers[i].getLatLng(), 13);
+  //         markers[i].openPopup();
+  //         window.setTimeout(run, 3000);
+  //     }
+  //     run();
+  // }
 
     map.markerLayer.setGeoJSON(geoJson);
   });
 }
 
 function update(race, race_name, names_in_race){
+  debugger
   geoJson = [];
   map.markerLayer.setGeoJSON(geoJson);
   fetch_data(race, race_name, names_in_race)
@@ -348,33 +335,32 @@ function banner(race, race_name){
   var width = ($(window).width()/length) - 40;
 
   race.forEach(function(candidate_arr, i){
-    var index = race.indexOf(candidate_arr);
-    candidate=candidate_arr[0];
+    var color = colors[race.indexOf(candidate_arr)];
+    var candidate = candidate_arr[0].substring(1);
 
-    var div = $("#banner").append("<li class='candidate " + candidate.substring(1)+ "' style='width: " + width + "'></li>");
-    $("."+candidate.substring(1)+"").append("<div class='"+ candidate.substring(1) +"_img img'></div>")
-    $("."+candidate.substring(1)+"_img").append("<img src='/static/candidates/"+ candidate.substring(1) +".jpg'>")
-    $("."+candidate.substring(1)+"_img").append("<div class='color_banner' style='background-color:"+ colors[index] +"'>")
+    $("#banner").append("<li class='candidate " + candidate + "' style='width: " + width + "'></li>");
+    $("." + candidate).append("<div class='"+ candidate +"_img img'></div>")
+    $("." + candidate + "_img").append("<img src='/static/candidates/" + candidate + ".jpg'>");
+    $("." + candidate + "_img").append("<div class='color_banner' style='background-color:"+ color + "'>")
 
 
-    $("."+candidate.substring(1)+"").append("<div class='"+candidate.substring(1)+"_text text_half'></div>")
-    $("."+candidate.substring(1)+"_text").append("<text>"+race_name[i]+"</text>")
-    $("."+candidate.substring(1)+"_text").append("<div class='stat_box'></div>")
-
-  })
+    $("." + candidate).append("<div class='" + candidate + "_text text_half'></div>");
+    $("." + candidate + "_text").append("<text>" + race_name[i] + "</text>");
+    $("." + candidate + "_text").append("<div class='stat_box'></div>");
+  });
 }
 
 function change_race(race, race_name, name_of_race){
-  banner(race, race_name)
-
-  update(race, name_of_race, race_name)
+  banner(race, race_name);
+  update(race, name_of_race, race_name);
 }
 
 // JQuery for UI
 
 $(document).ready(function () {
-    $("#races").bind('change', function(d) {
-        change_race(eval($(this).find('option:selected').attr("value")), eval($(this).find('option:selected').attr("value") + "_name"), $(this).find('option:selected').attr("value"));
+    $("#races").on('change', function(d) {
+      var race = $(this).find('option:selected').attr("value");
+      change_race(eval(race), eval(race + "_name"), race);
     });
 });
 
